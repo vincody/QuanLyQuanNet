1a9634f0ea7b94e030bbb1e0a52c75ac6de2d4b0
Tấn phát <hactequila2708@gmail.com>
2025-11-20 14:54:48 +0700

    tạo đơn giản 1 giao diện xem phim sử dụng webview2

--- QuanLyQuanNet/GUI/FormNgoai/FormKhach/XemPhim.Designer.cs
+++ QuanLyQuanNet/GUI/FormNgoai/FormKhach/XemPhim.Designer.cs
@@ -28,37 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            vlcControl1 = new Vlc.DotNet.Forms.VlcControl();
-            ((System.ComponentModel.ISupportInitialize)vlcControl1).BeginInit();
+            webViewPhim = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)webViewPhim).BeginInit();
             SuspendLayout();
             // 
-            // vlcControl1
+            // webViewPhim
             // 
-            vlcControl1.BackColor = Color.Black;
-            vlcControl1.Dock = DockStyle.Fill;
-            vlcControl1.Location = new Point(0, 0);
-            vlcControl1.Name = "vlcControl1";
-            vlcControl1.Size = new Size(800, 450);
-            vlcControl1.Spu = -1;
-            vlcControl1.TabIndex = 0;
-            vlcControl1.Text = "vlcControl1";
-            vlcControl1.VlcLibDirectory = null;
-            vlcControl1.VlcMediaplayerOptions = null;
+            webViewPhim.AllowExternalDrop = true;
+            webViewPhim.CreationProperties = null;
+            webViewPhim.DefaultBackgroundColor = Color.White;
+            webViewPhim.Dock = DockStyle.Fill;
+            webViewPhim.Location = new Point(0, 0);
+            webViewPhim.Name = "webViewPhim";
+            webViewPhim.Size = new Size(800, 450);
+            webViewPhim.TabIndex = 0;
+            webViewPhim.ZoomFactor = 1D;
             // 
             // XemPhim
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(vlcControl1);
+            Controls.Add(webViewPhim);
             Name = "XemPhim";
             Text = "XemPhim";
-            ((System.ComponentModel.ISupportInitialize)vlcControl1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)webViewPhim).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
-        private Vlc.DotNet.Forms.VlcControl vlcControl1;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webViewPhim;
     }
 }
