d58f8a37773d18c8452122f4516c695971b19db5
Tấn phát <hactequila2708@gmail.com>
2025-11-20 14:37:24 +0700

    rework lại giao diện phim

--- QuanLyQuanNet/GUI/FormNgoai/FormKhach/ListPhim.Designer.cs
+++ QuanLyQuanNet/GUI/FormNgoai/FormKhach/ListPhim.Designer.cs
@@ -43,7 +43,7 @@
             panelListPhim.BackColor = SystemColors.ControlDarkDark;
             panelListPhim.Location = new Point(0, 43);
             panelListPhim.Name = "panelListPhim";
-            panelListPhim.Size = new Size(800, 407);
+            panelListPhim.Size = new Size(875, 407);
             panelListPhim.TabIndex = 0;
             // 
             // textBoxSoTrang
@@ -93,7 +93,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(875, 450);
             Controls.Add(textBoxTimTen);
             Controls.Add(btnTru);
             Controls.Add(textBoxSoTrang);
