08a91ec239f09524707e67b703b6d9554fdbaeea
Tấn phát <hactequila2708@gmail.com>
2025-11-20 19:49:03 +0700

    feat: giao diện menu game cho khách hàng

--- QuanLyQuanNet/GUI/FormNgoai/FormKhach/MenuGames.Designer.cs
+++ QuanLyQuanNet/GUI/FormNgoai/FormKhach/MenuGames.Designer.cs
@@ -28,157 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MenuGames));
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
-            button4 = new Button();
-            button5 = new Button();
-            button6 = new Button();
-            button7 = new Button();
-            button8 = new Button();
-            flowLayoutPanel1 = new FlowLayoutPanel();
-            flowLayoutPanel1.SuspendLayout();
+            flowPanelGame = new FlowLayoutPanel();
             SuspendLayout();
             // 
-            // button1
+            // flowPanelGame
             // 
-            button1.BackColor = Color.Transparent;
-            button1.FlatAppearance.BorderSize = 0;
-            button1.FlatStyle = FlatStyle.Flat;
-            button1.Image = (Image)resources.GetObject("button1.Image");
-            button1.Location = new Point(3, 3);
-            button1.Name = "button1";
-            button1.Size = new Size(94, 69);
-            button1.TabIndex = 0;
-            button1.Text = "Valorant";
-            button1.TextImageRelation = TextImageRelation.ImageAboveText;
-            button1.UseVisualStyleBackColor = false;
-            // 
-            // button2
-            // 
-            button2.BackColor = Color.Transparent;
-            button2.FlatAppearance.BorderSize = 0;
-            button2.FlatStyle = FlatStyle.Flat;
-            button2.Image = (Image)resources.GetObject("button2.Image");
-            button2.Location = new Point(103, 3);
-            button2.Name = "button2";
-            button2.Size = new Size(94, 69);
-            button2.TabIndex = 1;
-            button2.Text = "CSGO";
-            button2.TextImageRelation = TextImageRelation.ImageAboveText;
-            button2.UseVisualStyleBackColor = false;
-            // 
-            // button3
-            // 
-            button3.BackColor = Color.Transparent;
-            button3.FlatAppearance.BorderSize = 0;
-            button3.FlatStyle = FlatStyle.Flat;
-            button3.Image = (Image)resources.GetObject("button3.Image");
-            button3.Location = new Point(203, 3);
-            button3.Name = "button3";
-            button3.Size = new Size(98, 61);
-            button3.TabIndex = 2;
-            button3.Text = "Genshit Impact";
-            button3.TextImageRelation = TextImageRelation.ImageAboveText;
-            button3.UseVisualStyleBackColor = false;
-            // 
-            // button4
-            // 
-            button4.BackColor = Color.Transparent;
-            button4.FlatAppearance.BorderSize = 0;
-            button4.FlatStyle = FlatStyle.Flat;
-            button4.Image = (Image)resources.GetObject("button4.Image");
-            button4.Location = new Point(307, 3);
-            button4.Name = "button4";
-            button4.Size = new Size(94, 69);
-            button4.TabIndex = 3;
-            button4.Text = "HSR";
-            button4.TextImageRelation = TextImageRelation.ImageAboveText;
-            button4.UseVisualStyleBackColor = false;
-            // 
-            // button5
-            // 
-            button5.BackColor = Color.Transparent;
-            button5.FlatAppearance.BorderSize = 0;
-            button5.FlatStyle = FlatStyle.Flat;
-            button5.Image = (Image)resources.GetObject("button5.Image");
-            button5.Location = new Point(407, 3);
-            button5.Name = "button5";
-            button5.Size = new Size(94, 69);
-            button5.TabIndex = 4;
-            button5.Text = "ZZZ";
-            button5.TextImageRelation = TextImageRelation.ImageAboveText;
-            button5.UseVisualStyleBackColor = false;
-            // 
-            // button6
-            // 
-            button6.BackColor = Color.Transparent;
-            button6.FlatAppearance.BorderSize = 0;
-            button6.FlatStyle = FlatStyle.Flat;
-            button6.Image = (Image)resources.GetObject("button6.Image");
-            button6.Location = new Point(507, 3);
-            button6.Name = "button6";
-            button6.Size = new Size(94, 69);
-            button6.TabIndex = 5;
-            button6.Text = "Wuthering waves";
-            button6.TextImageRelation = TextImageRelation.ImageAboveText;
-            button6.UseVisualStyleBackColor = false;
-            // 
-            // button7
-            // 
-            button7.BackColor = Color.Transparent;
-            button7.FlatAppearance.BorderSize = 0;
-            button7.FlatStyle = FlatStyle.Flat;
-            button7.Image = (Image)resources.GetObject("button7.Image");
-            button7.Location = new Point(607, 3);
-            button7.Name = "button7";
-            button7.Size = new Size(94, 61);
-            button7.TabIndex = 6;
-            button7.Text = "Steam";
-            button7.TextImageRelation = TextImageRelation.ImageAboveText;
-            button7.UseVisualStyleBackColor = false;
-            // 
-            // button8
-            // 
-            button8.BackColor = Color.Transparent;
-            button8.FlatAppearance.BorderSize = 0;
-            button8.FlatStyle = FlatStyle.Flat;
-            button8.Image = (Image)resources.GetObject("button8.Image");
-            button8.Location = new Point(707, 3);
-            button8.Name = "button8";
-            button8.Size = new Size(94, 61);
-            button8.TabIndex = 7;
-            button8.Text = "Discord";
-            button8.TextImageRelation = TextImageRelation.ImageAboveText;
-            button8.UseVisualStyleBackColor = false;
-            // 
-            // flowLayoutPanel1
-            // 
-            flowLayoutPanel1.AutoScroll = true;
-            flowLayoutPanel1.Controls.Add(button1);
-            flowLayoutPanel1.Controls.Add(button2);
-            flowLayoutPanel1.Controls.Add(button3);
-            flowLayoutPanel1.Controls.Add(button4);
-            flowLayoutPanel1.Controls.Add(button5);
-            flowLayoutPanel1.Controls.Add(button6);
-            flowLayoutPanel1.Controls.Add(button7);
-            flowLayoutPanel1.Controls.Add(button8);
-            flowLayoutPanel1.Dock = DockStyle.Fill;
-            flowLayoutPanel1.Location = new Point(0, 0);
-            flowLayoutPanel1.Name = "flowLayoutPanel1";
-            flowLayoutPanel1.Size = new Size(808, 450);
-            flowLayoutPanel1.TabIndex = 8;
+            flowPanelGame.AutoScroll = true;
+            flowPanelGame.Dock = DockStyle.Fill;
+            flowPanelGame.Location = new Point(0, 0);
+            flowPanelGame.Name = "flowPanelGame";
+            flowPanelGame.Size = new Size(808, 450);
+            flowPanelGame.TabIndex = 8;
             // 
             // MenuGames
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(808, 450);
-            Controls.Add(flowLayoutPanel1);
+            Controls.Add(flowPanelGame);
             Name = "MenuGames";
             Text = "Menu games";
-            flowLayoutPanel1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -192,6 +61,6 @@
         private Button button6;
         private Button button7;
         private Button button8;
-        private FlowLayoutPanel flowLayoutPanel1;
+        private FlowLayoutPanel flowPanelGame;
     }
 }
