96df63456a8f295aee98d1ef9d565ce45b43bd30
Hoài Phương <phantomdevil34@gmail.com>
2025-11-22 21:02:41 +0700

    giao diện nạp tiền vào tài khoản khách cho admin

--- QuanLyQuanNet/GUI/FormNgoai/FormAdmin/KHang.Designer.cs
+++ QuanLyQuanNet/GUI/FormNgoai/FormAdmin/KHang.Designer.cs
@@ -28,16 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle5 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle6 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle7 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle8 = new DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(KHang));
+            Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderEdges borderEdges2 = new Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderEdges();
             panel1 = new Panel();
             dataGridViewTK = new Bunifu.UI.WinForms.BunifuDataGridView();
             panel2 = new Panel();
             btnSuaThongTin = new Button();
             btnThemTaiKhoan = new Button();
             textBoxSDT = new TextBox();
+            btnNapTien = new Bunifu.UI.WinForms.BunifuButton.BunifuButton();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridViewTK).BeginInit();
             panel2.SuspendLayout();
@@ -49,7 +52,7 @@
             panel1.Controls.Add(dataGridViewTK);
             panel1.Location = new Point(0, 56);
             panel1.Name = "panel1";
-            panel1.Size = new Size(800, 394);
+            panel1.Size = new Size(820, 394);
             panel1.TabIndex = 0;
             // 
             // dataGridViewTK
@@ -59,21 +62,21 @@
             dataGridViewTK.AllowUserToDeleteRows = false;
             dataGridViewTK.AllowUserToResizeColumns = false;
             dataGridViewTK.AllowUserToResizeRows = false;
-            dataGridViewCellStyle1.BackColor = Color.FromArgb(248, 251, 255);
-            dataGridViewCellStyle1.ForeColor = Color.Black;
-            dataGridViewTK.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle5.BackColor = Color.FromArgb(248, 251, 255);
+            dataGridViewCellStyle5.ForeColor = Color.Black;
+            dataGridViewTK.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle5;
             dataGridViewTK.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             dataGridViewTK.BorderStyle = BorderStyle.None;
             dataGridViewTK.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
             dataGridViewTK.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
-            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle2.BackColor = Color.DodgerBlue;
-            dataGridViewCellStyle2.Font = new Font("Segoe UI Semibold", 11.75F, FontStyle.Bold);
-            dataGridViewCellStyle2.ForeColor = Color.White;
-            dataGridViewCellStyle2.SelectionBackColor = Color.FromArgb(24, 115, 204);
-            dataGridViewCellStyle2.SelectionForeColor = Color.White;
-            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.True;
-            dataGridViewTK.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle6.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle6.BackColor = Color.DodgerBlue;
+            dataGridViewCellStyle6.Font = new Font("Segoe UI Semibold", 11.75F, FontStyle.Bold);
+            dataGridViewCellStyle6.ForeColor = Color.White;
+            dataGridViewCellStyle6.SelectionBackColor = Color.FromArgb(24, 115, 204);
+            dataGridViewCellStyle6.SelectionForeColor = Color.White;
+            dataGridViewCellStyle6.WrapMode = DataGridViewTriState.True;
+            dataGridViewTK.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle6;
             dataGridViewTK.ColumnHeadersHeight = 40;
             dataGridViewTK.CurrentTheme.AlternatingRowsStyle.BackColor = Color.FromArgb(248, 251, 255);
             dataGridViewTK.CurrentTheme.AlternatingRowsStyle.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold);
@@ -93,14 +96,14 @@
             dataGridViewTK.CurrentTheme.RowsStyle.ForeColor = Color.Black;
             dataGridViewTK.CurrentTheme.RowsStyle.SelectionBackColor = Color.FromArgb(210, 232, 255);
             dataGridViewTK.CurrentTheme.RowsStyle.SelectionForeColor = Color.Black;
-            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle3.BackColor = Color.White;
-            dataGridViewCellStyle3.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold);
-            dataGridViewCellStyle3.ForeColor = Color.Black;
-            dataGridViewCellStyle3.SelectionBackColor = Color.FromArgb(210, 232, 255);
-            dataGridViewCellStyle3.SelectionForeColor = Color.Black;
-            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.False;
-            dataGridViewTK.DefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle7.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle7.BackColor = Color.White;
+            dataGridViewCellStyle7.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold);
+            dataGridViewCellStyle7.ForeColor = Color.Black;
+            dataGridViewCellStyle7.SelectionBackColor = Color.FromArgb(210, 232, 255);
+            dataGridViewCellStyle7.SelectionForeColor = Color.Black;
+            dataGridViewCellStyle7.WrapMode = DataGridViewTriState.False;
+            dataGridViewTK.DefaultCellStyle = dataGridViewCellStyle7;
             dataGridViewTK.Dock = DockStyle.Fill;
             dataGridViewTK.EnableHeadersVisualStyles = false;
             dataGridViewTK.GridColor = Color.FromArgb(221, 238, 255);
@@ -110,30 +113,31 @@
             dataGridViewTK.Location = new Point(0, 0);
             dataGridViewTK.Name = "dataGridViewTK";
             dataGridViewTK.ReadOnly = true;
-            dataGridViewCellStyle4.Alignment = DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle4.BackColor = SystemColors.Control;
-            dataGridViewCellStyle4.Font = new Font("Segoe UI", 9F);
-            dataGridViewCellStyle4.ForeColor = SystemColors.WindowText;
-            dataGridViewCellStyle4.SelectionBackColor = SystemColors.Highlight;
-            dataGridViewCellStyle4.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle4.WrapMode = DataGridViewTriState.True;
-            dataGridViewTK.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle8.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle8.BackColor = SystemColors.Control;
+            dataGridViewCellStyle8.Font = new Font("Segoe UI", 9F);
+            dataGridViewCellStyle8.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle8.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle8.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle8.WrapMode = DataGridViewTriState.True;
+            dataGridViewTK.RowHeadersDefaultCellStyle = dataGridViewCellStyle8;
             dataGridViewTK.RowHeadersVisible = false;
             dataGridViewTK.RowTemplate.Height = 40;
             dataGridViewTK.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            dataGridViewTK.Size = new Size(800, 394);
+            dataGridViewTK.Size = new Size(820, 394);
             dataGridViewTK.TabIndex = 0;
             dataGridViewTK.Theme = Bunifu.UI.WinForms.BunifuDataGridView.PresetThemes.Light;
             // 
             // panel2
             // 
+            panel2.Controls.Add(btnNapTien);
             panel2.Controls.Add(btnSuaThongTin);
             panel2.Controls.Add(btnThemTaiKhoan);
             panel2.Controls.Add(textBoxSDT);
             panel2.Dock = DockStyle.Top;
             panel2.Location = new Point(0, 0);
             panel2.Name = "panel2";
-            panel2.Size = new Size(800, 50);
+            panel2.Size = new Size(820, 50);
             panel2.TabIndex = 1;
             // 
             // btnSuaThongTin
@@ -171,11 +175,101 @@
             textBoxSDT.TabIndex = 0;
             textBoxSDT.TextChanged += textBoxSDT_TextChanged;
             // 
+            // btnNapTien
+            // 
+            btnNapTien.AllowAnimations = true;
+            btnNapTien.AllowMouseEffects = true;
+            btnNapTien.AllowToggling = false;
+            btnNapTien.AnimationSpeed = 200;
+            btnNapTien.AutoGenerateColors = false;
+            btnNapTien.AutoRoundBorders = false;
+            btnNapTien.AutoSizeLeftIcon = true;
+            btnNapTien.AutoSizeRightIcon = true;
+            btnNapTien.BackColor = Color.Transparent;
+            btnNapTien.BackColor1 = Color.FromArgb(51, 122, 183);
+            btnNapTien.BackgroundImage = (Image)resources.GetObject("btnNapTien.BackgroundImage");
+            btnNapTien.BorderStyle = Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderStyles.Solid;
+            btnNapTien.ButtonText = "Nạp tiền";
+            btnNapTien.ButtonTextMarginLeft = 0;
+            btnNapTien.ColorContrastOnClick = 45;
+            btnNapTien.ColorContrastOnHover = 45;
+            borderEdges2.BottomLeft = true;
+            borderEdges2.BottomRight = true;
+            borderEdges2.TopLeft = true;
+            borderEdges2.TopRight = true;
+            btnNapTien.CustomizableEdges = borderEdges2;
+            btnNapTien.DialogResult = DialogResult.None;
+            btnNapTien.DisabledBorderColor = Color.FromArgb(191, 191, 191);
+            btnNapTien.DisabledFillColor = Color.Empty;
+            btnNapTien.DisabledForecolor = Color.Empty;
+            btnNapTien.FocusState = Bunifu.UI.WinForms.BunifuButton.BunifuButton.ButtonStates.Pressed;
+            btnNapTien.Font = new Font("Segoe UI", 9F);
+            btnNapTien.ForeColor = Color.Black;
+            btnNapTien.IconLeft = null;
+            btnNapTien.IconLeftAlign = ContentAlignment.MiddleLeft;
+            btnNapTien.IconLeftCursor = Cursors.Default;
+            btnNapTien.IconLeftPadding = new Padding(11, 3, 3, 3);
+            btnNapTien.IconMarginLeft = 11;
+            btnNapTien.IconPadding = 10;
+            btnNapTien.IconRight = null;
+            btnNapTien.IconRightAlign = ContentAlignment.MiddleRight;
+            btnNapTien.IconRightCursor = Cursors.Default;
+            btnNapTien.IconRightPadding = new Padding(3, 3, 7, 3);
+            btnNapTien.IconSize = 25;
+            btnNapTien.IdleBorderColor = Color.Empty;
+            btnNapTien.IdleBorderRadius = 0;
+            btnNapTien.IdleBorderThickness = 0;
+            btnNapTien.IdleFillColor = Color.Empty;
+            btnNapTien.IdleIconLeftImage = null;
+            btnNapTien.IdleIconRightImage = null;
+            btnNapTien.IndicateFocus = false;
+            btnNapTien.Location = new Point(662, 6);
+            btnNapTien.Name = "btnNapTien";
+            btnNapTien.OnDisabledState.BorderColor = Color.FromArgb(191, 191, 191);
+            btnNapTien.OnDisabledState.BorderRadius = 15;
+            btnNapTien.OnDisabledState.BorderStyle = Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderStyles.Solid;
+            btnNapTien.OnDisabledState.BorderThickness = 1;
+            btnNapTien.OnDisabledState.FillColor = Color.FromArgb(204, 204, 204);
+            btnNapTien.OnDisabledState.ForeColor = Color.FromArgb(168, 160, 168);
+            btnNapTien.OnDisabledState.IconLeftImage = null;
+            btnNapTien.OnDisabledState.IconRightImage = null;
+            btnNapTien.onHoverState.BorderColor = Color.FromArgb(30, 150, 255);
+            btnNapTien.onHoverState.BorderRadius = 15;
+            btnNapTien.onHoverState.BorderStyle = Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderStyles.Solid;
+            btnNapTien.onHoverState.BorderThickness = 1;
+            btnNapTien.onHoverState.FillColor = Color.FromArgb(30, 150, 255);
+            btnNapTien.onHoverState.ForeColor = Color.White;
+            btnNapTien.onHoverState.IconLeftImage = null;
+            btnNapTien.onHoverState.IconRightImage = null;
+            btnNapTien.OnIdleState.BorderColor = Color.SteelBlue;
+            btnNapTien.OnIdleState.BorderRadius = 15;
+            btnNapTien.OnIdleState.BorderStyle = Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderStyles.Solid;
+            btnNapTien.OnIdleState.BorderThickness = 1;
+            btnNapTien.OnIdleState.FillColor = Color.AliceBlue;
+            btnNapTien.OnIdleState.ForeColor = Color.Black;
+            btnNapTien.OnIdleState.IconLeftImage = null;
+            btnNapTien.OnIdleState.IconRightImage = null;
+            btnNapTien.OnPressedState.BorderColor = Color.FromArgb(40, 96, 144);
+            btnNapTien.OnPressedState.BorderRadius = 15;
+            btnNapTien.OnPressedState.BorderStyle = Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderStyles.Solid;
+            btnNapTien.OnPressedState.BorderThickness = 1;
+            btnNapTien.OnPressedState.FillColor = Color.FromArgb(40, 96, 144);
+            btnNapTien.OnPressedState.ForeColor = Color.White;
+            btnNapTien.OnPressedState.IconLeftImage = null;
+            btnNapTien.OnPressedState.IconRightImage = null;
+            btnNapTien.Size = new Size(150, 39);
+            btnNapTien.TabIndex = 2;
+            btnNapTien.TextAlign = ContentAlignment.MiddleCenter;
+            btnNapTien.TextAlignment = HorizontalAlignment.Center;
+            btnNapTien.TextMarginLeft = 0;
+            btnNapTien.TextPadding = new Padding(0);
+            btnNapTien.UseDefaultRadiusAndThickness = true;
+            // 
             // KHang
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(820, 450);
             Controls.Add(panel2);
             Controls.Add(panel1);
             Name = "KHang";
@@ -196,5 +290,6 @@
         private Button btnThemTaiKhoan;
         private Button btnSuaThongTin;
         private Bunifu.UI.WinForms.BunifuDataGridView dataGridViewTK;
+        private Bunifu.UI.WinForms.BunifuButton.BunifuButton btnNapTien;
     }
 }
